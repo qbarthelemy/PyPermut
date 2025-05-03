5a1bb0a779b1a38c25a7e61ee8209026de8138cf
qbarthelemy <q.barthelemy@gmail.com>
2025-05-03 08:26:19 +0200

    add testing.yml file and change toml file

--- setup.py
+++ setup.py
@@ -8,13 +8,25 @@ setup_kwargs = {
     'author': 'Quentin Barthelemy',
     'url': 'https://github.com/qbarthelemy/PyPermut',
     'packages': ['pypermut'],
+    'platforms': 'any',
+    'python_requires': '>=3.9,<4.0',
     'install_requires': [
         'matplotlib>=3.0.0',
         'numpy>=1.16.5',
         'scipy>=1.6.0,<1.17.0',
-        'setuptools>=52.0.0'
     ],
-    'python_requires': '>=3.7,<4.0',
+    'extras_require': {
+        'docs': [
+            'sphinx',
+            'sphinx-gallery',
+            'numpydoc',
+            'scikit-learn',
+        ],
+        'tests': [
+            'flake8',
+            'pytest',
+        ],
+    },
 }
 
 
